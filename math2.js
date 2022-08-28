6a21d924f5ef9cd2a6ec83b8ba4a0b95b6704ea7
TUHIN <tuhin15-13388@diu.edu.bd>
2022-08-28 10:06:06 +0600

    random figer added

---
+++ math2.js
@@ -0,0 +1,25 @@
+// absolute number
+var number = -5;
+var absoluteNumber = Math.abs(number);
+console.log(absoluteNumber);
+
+// number round
+var number = 5.56839;
+var result = Math.round(number);
+console.log(result);
+
+//ceiling
+var number = 8.06839;
+var result = Math.ceil(number);
+console.log(result);
+
+//flooring
+
+var number = 10.96839;
+var result = Math.floor(number);
+console.log(result);
+
+//random value
+var number = Math.random()*100;
+var result = Math.round(number);
+console.log(result);
