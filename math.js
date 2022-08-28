d2fe74e03439fe80b93f68bb1a4b908c7436bcab
TUHIN <tuhin15-13388@diu.edu.bd>
2022-08-28 09:39:19 +0600

    math tricks added

---
+++ math.js
@@ -0,0 +1,48 @@
+// addition
+var price1 = 25;
+var price2 = 35;
+var total = price2 + price1;
+console.log(total);
+
+//subtratction
+var price1 = 25;
+var price2 = 35;
+var total = price2 - price1;
+console.log(total);
+
+//multiplication
+var price1 = 25;
+var price2 = 35;
+var total = price2 * price1;
+console.log(total);
+
+//divition
+var price1 = 25;
+var price2 = 35;
+var total = price2 / price1;
+console.log(total);
+
+var price1 = 25;
+var price2 = 35;
+var total = price2 % price1;
+console.log(total);
+
+// increment
+
+var price1 = 25;
+var price2 = 35;
+price2++;
+console.log(price2);
+
+// decrement
+
+var price1 = 25;
+var price2 = 35;
+price2--;
+console.log(price2);
+
+//white space
+var name = 'Tuhin';
+var name2 = 'Sarker';
+var result = name + ' ' + name2;
+console.log(result);
--- practice.js
+++ practice.js
@@ -1,3 +1,9 @@
-var number1 =25;
-var number2 = 15.5;
-console.log(number2);
+var number1 =0.1;
+var number2 = 0.2;
+//console.log(number2);
+//number2 = parseInt(number2);
+//console.log(number1 + number2);
+
+var total = number1 + number2;
+total = total.toFixed(3)
+console.log(total)
