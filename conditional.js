ea12680b4f57a2334e71141d25dd06b68bb63e3e
TUHIN <tuhin15-13388@diu.edu.bd>
2022-09-17 19:26:59 +0600

    conditional logic added

---
+++ conditional.js
@@ -0,0 +1,32 @@
+var biscuitPrice = 12;
+if(biscuitPrice<10){
+    console.log("I will eat the biscuits");
+}else{
+    console.log("without tea is better");
+}
+
+
+//nested condition
+var jobPaiso = true;
+var savingAmount = 500000;
+
+if(jobPaiso == true && savingAmount>200000){
+    console.log("Aso tomar patri khuji");
+}
+else if(jobPaiso==true){
+    console.log("acha koi din pore dekhbo");
+}
+else{
+    console.log("Tor kopale biya nai");
+}
+
+//again
+var jobPaiso = false;
+var savingAmount = 5000;
+
+if(jobPaiso == true || savingAmount>200000){
+    console.log("Aso tomar patri khuji");
+}
+else{
+    console.log("Tor kopale biya nai");
+}
